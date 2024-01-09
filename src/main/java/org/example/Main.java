b38217d4302992ff2db214cc7eb2593ae025e436
DeveloperSummit <anujs@TheDevSummit>
2024-01-09 23:47:28 +0530

    all dfata

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
 
        ApplicationContext  context = new ClassPathXmlApplicationContext("config.xml");
 
