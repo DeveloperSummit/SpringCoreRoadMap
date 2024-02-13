2eff3f1aae3dc3f749c50d97c488840fb80980aa
DeveloperSummit <anujs@TheDevSummit>
2024-02-13 23:58:56 +0530

    all

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
 
         System.out.println("Hello world!");
+        System.out.println("hi");
 
        ApplicationContext  context = new ClassPathXmlApplicationContext("config.xml");
 
